aa83abe70072c290286884bf36ba837833a155ec
bwrigh17 <bwrigh17@f2b27916-624c-4899-bd3f-7ce48d2512fa>
2017-02-14 20:29:30 +0000

    added square variables and constructor
    git-svn-id: svn+ssh://hive.cs.uwyo.edu/home/svn/cosc3011/c@24 f2b27916-624c-4899-bd3f-7ce48d2512fa

--- project1/trunk/game/Square.java
+++ project1/trunk/game/Square.java
@@ -1,5 +1,17 @@
 package game;
 
-public class Square {
+import java.awt.GridBagLayout;
 
+public class Square {
+	
+	private static int sideLength=5;
+	private int xLocation;
+	private int yLocation;
+
+	 public Square(int xLoc, int yLoc)
+	    {
+		 	xLocation = xLoc;
+		 	yLocation = yLoc;
+	    }
+	 
 }
