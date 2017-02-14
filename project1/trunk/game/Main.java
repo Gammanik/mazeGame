7dabdb76677b9ae83f7da4bb0e20f1cf908ccacf
bwrigh17 <bwrigh17@f2b27916-624c-4899-bd3f-7ce48d2512fa>
2017-02-14 20:10:55 +0000

    game window name
    git-svn-id: svn+ssh://hive.cs.uwyo.edu/home/svn/cosc3011/c@21 f2b27916-624c-4899-bd3f-7ce48d2512fa

--- project1/trunk/game/Main.java
+++ project1/trunk/game/Main.java
@@ -25,7 +25,7 @@ public class Main
   public static void main(String[] args)
   {
     // This is the play area
-    GameWindow game = new GameWindow("Group X aMaze");
+    GameWindow game = new GameWindow("Group C Maze");
     
     // have to override the default layout to reposition things!!!!!!!
 
