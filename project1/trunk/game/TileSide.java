c1a1ce155dd5bfc3d2ad6a6731adc09cc425d199
pfechtme <pfechtme@f2b27916-624c-4899-bd3f-7ce48d2512fa>
2017-02-21 02:40:33 +0000

    added a comment
    git-svn-id: svn+ssh://hive.cs.uwyo.edu/home/svn/cosc3011/c@58 f2b27916-624c-4899-bd3f-7ce48d2512fa

--- project1/trunk/game/TileSide.java
+++ project1/trunk/game/TileSide.java
@@ -10,8 +10,28 @@ public class TileSide extends JButton{
 	
 	public TileSide(String num)
 	{	
-		super(num);
+		super(num); // calls the default JButton 1 parameter method and puts
+		            // passed in text (e.g. num on that button)
 		setBackground(new Color(242,109, 91));
 		setOpaque(true);
 	}
 }
+
+
+/*        the UML 
+ * 
+ *
+ *        +----------------------+
+ *        |     TileSide         |
+ *        +----------------------+
+ *        |   -isEmpty :boolean  |
+ *        |   -tileId  :int      |
+ *        +----------------------+
+ *        |  «cons» Tile(id:int) |
+ *        |  +getTileId():int    |
+ *        |  +getStatus():bool   |
+ *        +----------------------+
+ * 
+ * 
+ * 
+ */
