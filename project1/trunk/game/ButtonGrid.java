f9fb2c455a84cce4e116ed6e15ee573515ae86d8
bmille64 <bmille64@f2b27916-624c-4899-bd3f-7ce48d2512fa>
2017-02-22 22:34:22 +0000

    Made some changes to simplify the code. Re-uploaded.
    git-svn-id: svn+ssh://hive.cs.uwyo.edu/home/svn/cosc3011/c@70 f2b27916-624c-4899-bd3f-7ce48d2512fa

--- project1/trunk/game/ButtonGrid.java
+++ project1/trunk/game/ButtonGrid.java
@@ -10,31 +10,31 @@ import com.sun.glass.ui.Menu;
 import java.awt.*;  // doesn't this bring in all awt so next 2 lines redundant?
 
 
+
 public class ButtonGrid extends JPanel {
 
     public static final long serialVersionUID=1;
     
     JFrame frame = new JFrame();
-    int t = 4;// sets the dimensions of the button grid
+    
 
     public ButtonGrid() { // constructor
-        frame.setContentPane(this);
-        setLayout(new GridLayout(t, t, 3, 3));
-        addButtons(t);
+      
+        setLayout(new GridLayout(4, 4, 4, 4));
+        addButtons();
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-
     }
 
 
 
-    private void addButtons(int t) {
-        JButton grid;
-        for (int y = 0; y < t; y++) {
-            for (int x = 0; x < t; x++) {
-                grid = new JButton(x + "," + y); // creates new button
-                grid.setPreferredSize(new Dimension(55, 55));
+    
+    
+    private void addButtons() 
+    {
+        JButton grid = new JButton();
+        for (int y = 0; y < 4; y++) {
+            for (int x = 0; x < 4; x++) {
+                grid.setPreferredSize(new Dimension(40, 40));
                 add(grid); // adds button to grid
 
             }
@@ -54,7 +54,7 @@ public class ButtonGrid extends JPanel {
  *        +---------------------------------------+
  *        |  «cons» ButtonGrid()                  |
  *        |  + setUp() : void                     |
- *        |  + addButtons(int t) : void           |
+ *        |  + addButtons() : void                |
  *        |  + addSideTiles(): void               | 
  *        +---------------------------------------+
  * 
