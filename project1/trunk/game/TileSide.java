a6925b16f0fe8f6e0f3b2901d21d910af5df2afe
bwrigh17 <bwrigh17@f2b27916-624c-4899-bd3f-7ce48d2512fa>
2017-02-27 03:13:58 +0000

    git-svn-id: svn+ssh://hive.cs.uwyo.edu/home/svn/cosc3011/c@75 f2b27916-624c-4899-bd3f-7ce48d2512fa

--- project1/trunk/game/TileSide.java
+++ project1/trunk/game/TileSide.java
@@ -1,6 +1,8 @@
 package game;
 
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JLabel;
 //import javax.swing.JPanel;
@@ -16,5 +18,39 @@ public TileSide(String num)
 super(num);
 setBackground(Color.WHITE);
 setOpaque(true);
+//added by blake: basic mouse listener
+this.addMouseListener(new MouseListener()
+{
+ @Override
+ public void mouseClicked(MouseEvent arg0) {
+   System.out.println("clicked tile");
+   setBackground(Color.CYAN);
+   
+ }
+
+ @Override
+ public void mouseEntered(MouseEvent arg0) {
+   // TODO Auto-generated method stub
+   
+ }
+
+ @Override
+ public void mouseExited(MouseEvent arg0) {
+   // TODO Auto-generated method stub
+   
+ }
+
+ @Override
+ public void mousePressed(MouseEvent arg0) {
+   // TODO Auto-generated method stub
+   
+ }
+
+ @Override
+ public void mouseReleased(MouseEvent arg0) {
+   // TODO Auto-generated method stub
+   
+ }
+});
 }
 }
