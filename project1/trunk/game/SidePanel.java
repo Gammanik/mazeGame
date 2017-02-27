f9841773e2539848e85f4b69ac5b1e381001acf4
bwrigh17 <bwrigh17@f2b27916-624c-4899-bd3f-7ce48d2512fa>
2017-02-27 03:01:38 +0000

    added sidetile mouse listener
    git-svn-id: svn+ssh://hive.cs.uwyo.edu/home/svn/cosc3011/c@73 f2b27916-624c-4899-bd3f-7ce48d2512fa

--- project1/trunk/game/SidePanel.java
+++ project1/trunk/game/SidePanel.java
@@ -4,6 +4,10 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
 
@@ -32,6 +36,40 @@ public class SidePanel extends JPanel {
 			tile.setMinimumSize(new Dimension(50,50));
 			tile.setPreferredSize(new Dimension(100,100));
 			this.add(tile,cons);
+			
+			//added by blake: basic mouse listener
+			this.addMouseListener(new MouseListener()
+       {
+        @Override
+        public void mouseClicked(MouseEvent arg0) {
+          System.out.println("clicked tile");
+          
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent arg0) {
+          // TODO Auto-generated method stub
+          
+        }
+
+        @Override
+        public void mouseExited(MouseEvent arg0) {
+          // TODO Auto-generated method stub
+          
+        }
+
+        @Override
+        public void mousePressed(MouseEvent arg0) {
+          // TODO Auto-generated method stub
+          
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent arg0) {
+          // TODO Auto-generated method stub
+          
+        }
+       });
 		}
 	  
 	}
