aa4c8b4f6002d11307c4312ee0a6b6ad5ef4da13
bwrigh17 <bwrigh17@f2b27916-624c-4899-bd3f-7ce48d2512fa>
2017-02-27 03:14:30 +0000

    moved actionlistener to individual tiles
    git-svn-id: svn+ssh://hive.cs.uwyo.edu/home/svn/cosc3011/c@76 f2b27916-624c-4899-bd3f-7ce48d2512fa

--- project1/trunk/game/SidePanel.java
+++ project1/trunk/game/SidePanel.java
@@ -1,5 +1,6 @@
 package game;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -36,40 +37,6 @@ public class SidePanel extends JPanel {
 			tile.setMinimumSize(new Dimension(50,50));
 			tile.setPreferredSize(new Dimension(100,100));
 			this.add(tile,cons);
-			
-			//added by blake: basic mouse listener
-			this.addMouseListener(new MouseListener()
-       {
-        @Override
-        public void mouseClicked(MouseEvent arg0) {
-          System.out.println("clicked tile");
-          
-        }
-
-        @Override
-        public void mouseEntered(MouseEvent arg0) {
-          // TODO Auto-generated method stub
-          
-        }
-
-        @Override
-        public void mouseExited(MouseEvent arg0) {
-          // TODO Auto-generated method stub
-          
-        }
-
-        @Override
-        public void mousePressed(MouseEvent arg0) {
-          // TODO Auto-generated method stub
-          
-        }
-
-        @Override
-        public void mouseReleased(MouseEvent arg0) {
-          // TODO Auto-generated method stub
-          
-        }
-       });
 		}
 	  
 	}
