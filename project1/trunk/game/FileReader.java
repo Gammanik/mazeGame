01d98d83bdb45a375da8926fae7b5c4f3786c733
bmille64 <bmille64@f2b27916-624c-4899-bd3f-7ce48d2512fa>
2017-03-21 18:46:52 +0000

    Added .mze
    git-svn-id: svn+ssh://hive.cs.uwyo.edu/home/svn/cosc3011/c@108 f2b27916-624c-4899-bd3f-7ce48d2512fa

---
+++ project1/trunk/game/FileReader.java
@@ -0,0 +1,12 @@
+package game;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileReader {
+  FileInputStream input = null;
+  FileOutputStream output = null;
+  input = new FileInputStream("default-1.mze");
+  
+
+}
